e50dc0f9f7f9249cf72f2b9273bf2d019e30201c
talfyproject <mi.labianca@libero.it>
2025-07-29 15:25:51 +0200

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const User = sequelize.define('User', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  userType: { type: DataTypes.ENUM('candidate', 'company'), allowNull: false }
+});
+
+module.exports = User;
