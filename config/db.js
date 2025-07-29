3e129fe7a95597e9ba9d7d034a38112085ce3efe
talfyproject <mi.labianca@libero.it>
2025-07-29 15:25:06 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,18 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  logging: false
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('✅ PostgreSQL connected successfully!');
+  } catch (error) {
+    console.error('❌ DB connection failed:', error);
+  }
+})();
+
+module.exports = sequelize;
